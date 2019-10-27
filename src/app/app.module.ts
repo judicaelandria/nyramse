b21c6e212cf6dd0ef5954c24f08dc8645a522e9d
Neuronattor <44155065+Neuronattor@users.noreply.github.com>
2019-10-27 07:08:01 +0300

    Add call number

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NgCalendarModule } from 'ionic2-calendar';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CallNumber } from '@ionic-native/call-number/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [NgCalendarModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
+    CallNumber,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
--- src/app/pages/reservation/reservation.page.ts
+++ src/app/pages/reservation/reservation.page.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Map, tileLayer, marker, icon, Marker,Routing, latLng  } from 'leaflet';
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { Map, tileLayer, marker, icon, Marker, Routing, latLng } from 'leaflet';
 const iconBlueRetinaUrl = 'assets/leaflet/marker-icon-2x.fw.png';
 const iconBlueUrl = 'assets/leaflet/marker-icon-2x.fw.png';
 const iconYouRetinaUrl = 'assets/leaflet/pulse.gif';
 const iconYouUrl = 'assets/leaflet/pulse.gif';
 const shadowUrl = 'assets/leaflet/marker-shadow.png';
 import 'leaflet-routing-machine';
+import { ActionSheetController } from '@ionic/angular';
 
 const iconBlue = icon({
   iconRetinaUrl: iconBlueRetinaUrl,
   iconUrl: iconBlueUrl,
   shadowUrl,
-  iconSize: [38,50],
+  iconSize: [38, 50],
   iconAnchor: [22, 51],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
@@ -21,7 +23,7 @@ const iconYou = icon({
   iconRetinaUrl: iconYouRetinaUrl,
   iconUrl: iconYouUrl,
   shadowUrl,
-  iconSize: [38,50],
+  iconSize: [38, 50],
   iconAnchor: [22, 51],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
@@ -35,7 +37,7 @@ const iconYou = icon({
 export class ReservationPage implements OnInit {
   map: Map;
   eleves = [];
-  constructor() { }
+  constructor(public actionSheetController: ActionSheetController, private callNumber: CallNumber) { }
 
   ngOnInit() {
   }
@@ -50,10 +52,10 @@ export class ReservationPage implements OnInit {
     }).addTo(this.map);
 
     fetch('./assets/data/eleves.json').then(res => res.json())
-    .then(json => {
-      this.eleves = json.eleves;
-      this.leafletMap();
-    });
+      .then(json => {
+        this.eleves = json.eleves;
+        this.leafletMap();
+      });
     this.drawLine();
   }
 
@@ -63,12 +65,17 @@ export class ReservationPage implements OnInit {
     mrk = marker([-18.848334, 47.479791], iconYou).addTo(this.map)
       .bindPopup("C'est vous", 'mapContent')
       .openPopup();
-  
+
     for (const eleve of this.eleves) {
       Marker.prototype.options.icon = iconBlue;
       mrk = marker([eleve.lat, eleve.long], iconBlue).addTo(this.map)
         .bindPopup(eleve.nom)
         .openPopup();
+
+      mrk.on("click", (event) => {
+        var clickedMarker = event.layer;
+        this.presentActionSheet();
+      });
     }
   }
 
@@ -76,7 +83,7 @@ export class ReservationPage implements OnInit {
     this.map.remove();
   }
 
-  myPosition(){
+  myPosition() {
     this.map.setView([-18.848647, 47.480242], 18, {
       "animate": true,
       "pan": {
@@ -89,14 +96,37 @@ export class ReservationPage implements OnInit {
   drawLine() {
     new Routing.Control({
       showAlternatives: false,
-      lineOptions: {styles: [{color: '#E77E22', weight: 7}]},
+      lineOptions: { styles: [{ color: '#E77E22', weight: 7 }] },
       fitSelectedRoutes: false,
       show: true,
       routeWhileDragging: true,
       waypoints: [
-        latLng(-18.848475,  47.480187),
-          latLng(-18.848334, 47.479791)
+        latLng(-18.848475, 47.480187),
+        latLng(-18.848334, 47.479791)
       ]
     }).addTo(this.map)
   }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Rija Nifaliana',
+      buttons: [{
+        text: 'Apeller',
+        role: 'destructive',
+        icon: 'call',
+        handler: () => {
+          this.dial();
+        }
+      }]
+    });
+
+    await actionSheet.present();
+  }
+
+  dial() {
+    this.callNumber.callNumber("0340960616", true)
+      .then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
+  }
+
 }
